d132fe58c79c0f225f3c283cbbb222ad0cca9561
Srishty Sharma <84490597+srishty1101@users.noreply.github.com>
2021-10-04 23:15:47 +0530

    Create quiz.css

---
+++ quiz.css
@@ -0,0 +1,58 @@
+body{
+    color: blanchedalmond;
+    font-size: 2rem;
+}
+.choice-container{
+    display:flex;
+    margin-bottom: 0.8rem;
+    width: 100%;
+    border-radius: 4px;
+    background-color:#9388A2 ;
+    font-size: 3rem;
+    min-width: 80rem;
+}
+.choice-container:hover{
+    cursor: pointer;
+    box-shadow: 0 0.4rem 1.4rem 0 ( #695E93 , 0.5);
+    transform: scale(1.02);
+    transform: transform 100ns;
+
+}
+.choice-prefix{
+    padding: 2rem 2.5rem;
+    color:white;
+}
+.choice-text{
+    padding:2rem;
+    width:100%;
+}
+.correct{
+    background:  linear-gradient(32de , rgb(11,223,36)0%, rgb(41,232,111)100%);
+}
+.incorrect{
+    background:  linear-gradient(32de , rgb(230,29,29,1)0%, rgb(224,11,11,1)100%);
+}
+#hud{
+    display: flex;
+    justify-content: space-between;
+}
+.hud-prefix{
+    text-align: center;
+    font-size: 2rem;
+}
+.hud-main-text{
+    text-align: center;
+}
+#progressbar{
+    width:2rem;
+    height: 3rem;
+    border: 0.2rem solid rgb(11,223,36);
+    margin-top: 2rem;
+    overflow: hidden;
+
+}
+#progressbarfull{
+    height: 100%;
+    background: rgb(11,223,36);
+    width: 0%;
+}
